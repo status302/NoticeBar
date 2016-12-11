926dfc183e1616f8998d752fca0953ac27995ad7
Qiuncheng <qiuncheng@gmail.com>
2016-12-11 23:53:22 +0800

    update annotations.

--- NoticeBar/NoticeBar.h
+++ NoticeBar/NoticeBar.h
@@ -1,9 +1,15 @@
 //
-//  NoticeBar.h
-//  NoticeBar
+//  Created by Qiun Cheng on 2016/12/10.
+//  Copyright © 2016年 http://qiuncheng.com. All rights reserved.
 //
-//  Created by 程庆春 on 2016/12/10.
-//  Copyright © 2016年 qiuncheng.com. All rights reserved.
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
 
 #import <UIKit/UIKit.h>
